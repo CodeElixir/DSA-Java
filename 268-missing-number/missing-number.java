e0882afcc0946ed681e8dbc61946381d41db69a5
Sandeep <sandeepg2404@gmail.com>
2024-03-19 21:57:33 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (55.39%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,9 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        // Using sum
+
         // int n = nums.length;
         // int targetSum = (n * (n + 1)) / 2;
         // int sum = Arrays.stream(nums).sum();
         // return targetSum - sum;
+
+        // Using bitwise
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
             res = res ^ (i + 1) ^ nums[i];
