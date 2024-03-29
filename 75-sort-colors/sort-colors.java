e82e31573a514c07269646f983ae9629aca95ef8
Sandeep <sandeepg2404@gmail.com>
2024-03-29 20:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (12.55%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,25 +1,46 @@
 class Solution {
     public void sortColors(int[] nums) {
         // Brute Force
-        int[] temp = new int[3];
-        for (int ele : nums) {
-            temp[ele]++;
-        }
+        // int[] temp = new int[3];
+        // for (int ele : nums) {
+        //     temp[ele]++;
+        // }
 
-        int i = 0;
-        while (temp[0] != 0) {
-            nums[i++] = 0;
-            temp[0]--;
-        }
+        // int i = 0;
+        // while (temp[0] != 0) {
+        //     nums[i++] = 0;
+        //     temp[0]--;
+        // }
 
-        while (temp[1] != 0) {
-            nums[i++] = 1;
-            temp[1]--;
-        }
+        // while (temp[1] != 0) {
+        //     nums[i++] = 1;
+        //     temp[1]--;
+        // }
 
-        while (temp[2] != 0) {
-            nums[i++] = 2;
-            temp[2]--;
+        // while (temp[2] != 0) {
+        //     nums[i++] = 2;
+        //     temp[2]--;
+        // }
+
+        // Efficient
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
         }
     }
 }
