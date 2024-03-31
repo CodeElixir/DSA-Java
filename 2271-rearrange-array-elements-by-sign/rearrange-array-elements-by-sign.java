d7f3315af1212ad54385de0da129cc6f51f76f74
Sandeep <sandeepg2404@gmail.com>
2024-03-31 13:05:53 +0530

    Time: 3 ms (100.00%) | Memory: 77 MB (57.00%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,25 +1,39 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] res = new int[nums.length];
-        int[] temp = new int[nums.length];
-        int i = 0;
-        int j = nums.length / 2;
-        int k = 0;
-        while (k < nums.length) {
-            if (nums[k] < 0) {
-                temp[j++] = nums[k];
+        // Brute force
+        // int[] res = new int[nums.length];
+        // int[] temp = new int[nums.length];
+        // int i = 0;
+        // int j = nums.length / 2;
+        // int k = 0;
+        // while (k < nums.length) {
+        //     if (nums[k] < 0) {
+        //         temp[j++] = nums[k];
+        //     } else {
+        //         temp[i++] = nums[k];
+        //     }
+        //     k++;
+        // }
+        // i = 0;
+        // j = nums.length / 2;
+        // k = 0;
+        // while (k < nums.length) {
+        //     res[k] = temp[i++];
+        //     res[k + 1] = temp[j++];
+        //     k = k + 2;
+        // }
+        // return res;
+        int[] res =  new int[nums.length];
+        int positive = 0;
+        int negative = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                res[negative] = nums[i];
+                negative = negative + 2;
             } else {
-                temp[i++] = nums[k];
+                res[positive] = nums[i];
+                positive = positive + 2;               
             }
-            k++;
-        }
-        i = 0;
-        j = nums.length / 2;
-        k = 0;
-        while (k < nums.length) {
-            res[k] = temp[i++];
-            res[k + 1] = temp[j++];
-            k = k + 2;
         }
         return res;
     }
