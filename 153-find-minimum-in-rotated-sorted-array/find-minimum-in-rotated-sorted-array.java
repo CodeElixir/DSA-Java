bd6cf4945270f73a79d24a891a61ee0591b7313b
Sandeep <sandeepg2404@gmail.com>
2024-04-03 13:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (69.49%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,17 +1,34 @@
 class Solution {
     public int findMin(int[] nums) {
+        // int low = 0;
+        // int high = nums.length - 1;
+        // while (low <= high) {
+        //     int mid = low + ((high - low) / 2);
+        //     if ((mid == 0 || nums[mid] < nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] < nums[mid + 1])) {
+        //         return nums[mid];
+        //     } else if (nums[mid] >= nums[low] && nums[mid] >= nums[high]) {
+        //         low = mid + 1;
+        //     } else {
+        //         high = mid - 1;
+        //     }
+        // }
+        // return -1;
+        
         int low = 0;
         int high = nums.length - 1;
+        int min = Integer.MAX_VALUE;
         while (low <= high) {
             int mid = low + ((high - low) / 2);
-            if ((mid == 0 || nums[mid] < nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] < nums[mid + 1])) {
-                return nums[mid];
-            } else if (nums[mid] >= nums[low] && nums[mid] >= nums[high]) {
+            if (nums[mid] >= nums[low]) {
+                // Left half is sorted. Pick min and eliminate this half.
+                min = Math.min(nums[low], min);
                 low = mid + 1;
             } else {
+                // Right half is sorted. Pick min and eliminate this half.
+                min = Math.min(nums[mid], min);
                 high = mid - 1;
             }
         }
-        return -1;
+        return min;
     }
 }
