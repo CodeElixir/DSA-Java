c414ef47ebf037e715a09d5945fcaa1fa537bc4d
Sandeep <sandeepg2404@gmail.com>
2024-04-06 22:06:00 +0530

    Time: 2 ms (99.71%) | Memory: 42.4 MB (38.41%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//         StringBuilder res = new StringBuilder();
+//         int openCnt = 0;
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if (ch == '(') {
+//                 openCnt++;
+//                 if (openCnt > 1) res.append(ch);
+//             } else {
+//                 if (openCnt > 1) res.append(ch);
+//                 openCnt--;
+//             }
+//         }
+//         return res.toString();
+//     }
+// }
 class Solution {
-    public String removeOuterParentheses(String s) {
-        StringBuilder res = new StringBuilder();
-        int openCnt = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(') {
-                openCnt++;
-                if (openCnt > 1) res.append(ch);
+        public String removeOuterParentheses(String S) {
+        StringBuilder s = new StringBuilder();
+        int opened = 0;
+        for (char c : S.toCharArray()) {
+            // if (c == '(' && opened++ > 0) s.append(c);
+            // if (c == ')' && opened-- > 1) s.append(c);
+            if(c == '(') {
+                if(opened > 0) s.append(c);
+                opened++;
             } else {
-                if (openCnt > 1) res.append(ch);
-                openCnt--;
+                if(opened > 1) s.append(c);
+                opened--;
             }
         }
-        return res.toString();
+        return s.toString();
     }
 }
