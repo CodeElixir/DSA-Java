e3ed703b878c8bc04e65d5ab15f770b3876f0755
Sandeep <sandeepg2404@gmail.com>
2024-04-15 17:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (91.63%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,41 +10,33 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+
         if (head == null || head.next == null) {
             return head;
         }
-        int len = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            curr = curr.next;
+        int len = 1;
+        ListNode tail = head;
+        ListNode newHead = head;
+        ListNode newTail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
             len++;
         }
+
         k = k % len;
         if (k == 0) {
-            return head; 
+            return head;
         }
-        head = reverseKNodes(head, -1).get(0);
-        curr = head;
-        List<ListNode> list = reverseKNodes(curr, k);
-        ListNode newHead = list.get(0);
-        head.next = reverseKNodes(list.get(1), -1).get(0);
-        return newHead;
-    }
-    
-    private List<ListNode> reverseKNodes(ListNode head, int k) {
-        List<ListNode> list = new ArrayList<>(2);
-        int count = 0;
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null && (k == -1 || count < k)) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+
+        tail.next = head;
+        int count = 1;
+        while (newTail != null && count < (len - k)) {
+            newTail = newTail.next;
             count++;
         }
-        list.add(prev);
-        list.add(curr);
-        return list;
+        newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
     }
 }
