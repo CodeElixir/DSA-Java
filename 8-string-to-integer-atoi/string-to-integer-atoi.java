159f33c8b0f53ab65eb169372c1ec3224dbf9719
Sandeep <sandeepg2404@gmail.com>
2024-04-16 17:45:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (85.34%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,32 +1,56 @@
 class Solution {
     public int myAtoi(String s) {
-        long result = 0l;
-        s = s.trim();
-        boolean isNegative = false;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (i == 0 && (ch == '+' || ch == '-')) {
-                isNegative = (ch == '-') ? true : false;
-            } else {
-                if (!Character.isDigit(ch)) {
-                    break;
-                }
+        // long result = 0l;
+        // s = s.trim();
+        // boolean isNegative = false;
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     if (i == 0 && (ch == '+' || ch == '-')) {
+        //         isNegative = (ch == '-') ? true : false;
+        //     } else {
+        //         if (!Character.isDigit(ch)) {
+        //             break;
+        //         }
 
-                int digit = Character.digit(ch, 10);
-                result = Math.abs((result * 10)) + digit;
-                if (isNegative) {
-                    result = -result;
-                }
+        //         int digit = Character.digit(ch, 10);
+        //         result = Math.abs((result * 10)) + digit;
+        //         if (isNegative) {
+        //             result = -result;
+        //         }
 
-                if (result < Integer.MIN_VALUE) {
-                    return Integer.MIN_VALUE;
-                }
+        //         if (result < Integer.MIN_VALUE) {
+        //             return Integer.MIN_VALUE;
+        //         }
 
-                if (result > Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
-                } 
-            }
+        //         if (result > Integer.MAX_VALUE) {
+        //             return Integer.MAX_VALUE;
+        //         } 
+        //     }
+        // }
+        // return (int)result;
+
+        int sign = 1;
+        int res = 0;
+        int i = 0;
+
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
         }
-        return (int)result;
+
+        // Check for sign.
+        if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            sign = (s.charAt(i++) == '-') ? -1 : 1;
+        }
+
+        // Check for a digit.
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                // Check overflow
+                if (res > Integer.MAX_VALUE / 10 || 
+                (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                    return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                res = (res * 10) + (s.charAt(i++) - '0');
+        }
+        return res * sign;
     }
 }
