eb108e49523ada43393d71a91d5e192ef0841a3b
Sandeep <sandeepg2404@gmail.com>
2024-04-26 19:49:27 +0530

    Time: 1 ms (99.90%) | Memory: 45 MB (41.07%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -7,33 +7,6 @@ class Solution {
         return permutations;   
     }
 
-    // Brute force
-    // private void permuteHelper(int pos, int[] nums, Set<List<Integer>> permutations) {
-    //     if (pos == nums.length) {
-    //         List<Integer> list = new ArrayList<>();
-    //         for (int ele : nums) {
-    //             list.add(ele);
-    //         }
-    //         permutations.add(list);
-    //         return;
-    //     }
-
-    //     for (int i = pos; i < nums.length; i++) {
-    //         if (i > pos && nums[i] == nums[pos]) {
-    //             continue;
-    //         }
-    //         swap(pos, i, nums);
-    //         permuteHelper(pos + 1, nums, permutations);
-    //         swap(pos, i, nums);
-    //     }
-    // }
-
-    // private void swap(int i, int j, int[] nums) {
-    //     int t = nums[i];
-    //     nums[i] = nums[j];
-    //     nums[j] = t;
-    // }
-
     private void permuteHelper(int[] nums, boolean[] visited, List<Integer> permutation, List<List<Integer>> permutations) {
         if (permutation.size() == nums.length) {
             permutations.add(new ArrayList<>(permutation));
