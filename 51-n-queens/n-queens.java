98e305d5c9f82569eb38674edcacca95c9ba7c0d
Sandeep <sandeepg2404@gmail.com>
2024-04-28 18:52:02 +0530

    Time: 1 ms (99.60%) | Memory: 44.8 MB (50.36%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,11 +1,16 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> configurations = new ArrayList<>();
-        solveNQueensHelper(0, new int[n][n], n, configurations);
+        // Optimisation arrays
+        int[] col = new int[n];
+        int[] topLeft = new int[2 * n];
+        int[] topRight = new int[2 * n];
+        solveNQueensHelper(0, new int[n][n], n, configurations, col, topLeft, topRight);
         return configurations;
     }
 
-    private void solveNQueensHelper(int i, int[][] configuration, int n, List<List<String>> configurations) {
+    private void solveNQueensHelper(int i, int[][] configuration, int n, List<List<String>> configurations, 
+    int[] col, int[] topLeft, int[] topRight) {
         if (i == n) {
             List<String> res = new ArrayList<>();
             for (int j = 0; j < n; j++) {
@@ -21,50 +26,17 @@ class Solution {
         }
 
         for (int j = 0; j < n; j++) {
-            if (isSafe(i, j, configuration, n)) {
+            if (isSafe(i, j, n, col, topLeft, topRight)) {
                 configuration[i][j] = 1;
-                solveNQueensHelper(i + 1, configuration, n, configurations);
+                col[j] = topLeft[(i - j) + (n - 1)] = topRight[i + j] = 1;
+                solveNQueensHelper(i + 1, configuration, n, configurations, col, topLeft, topRight);
                 configuration[i][j] = 0;
+                col[j] = topLeft[(i - j) + (n - 1)] = topRight[i + j] = 0;
             }
         }
     }
 
-    private boolean isSafe(int i, int j, int[][] configuration, int n) {
-        int tempI = i;
-        int tempJ = j;
-        
-        // UP
-        while (tempI >= 0) {
-            if (configuration[tempI][tempJ] == 1) {
-                return false;
-            }
-            tempI--;
-        }
-
-        tempI = i;
-        tempJ = j;
-
-        // TOP RIGHT
-        while (tempI >= 0 && tempJ <= n - 1) {
-            if (configuration[tempI][tempJ] == 1) {
-                return false;
-            }
-            tempI--;
-            tempJ++;
-        }
-
-        tempI = i;
-        tempJ = j;
-
-        // TOP LEFT
-        while (tempI >= 0 && tempJ >= 0) {
-            if (configuration[tempI][tempJ] == 1) {
-                return false;
-            }
-            tempI--;
-            tempJ--;
-        }
-
-        return true;
+    private boolean isSafe(int i, int j, int n, int[] col, int[] topLeft, int[] topRight) {
+        return col[j] == 0 && topLeft[(i - j) + (n - 1)] == 0 && topRight[i + j] == 0;
     }
 }
