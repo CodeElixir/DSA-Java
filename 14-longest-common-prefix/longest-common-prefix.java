21c1534f648fce486b94f228d5c5f8d69a35b8ed
Sandeep <sandeepg2404@gmail.com>
2024-05-17 19:10:19 +0530

    Time: 1 ms (75.57%) | Memory: 41.8 MB (21.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -15,35 +15,35 @@ class Solution {
         // }
         // return builder.toString();
         
-        // StringBuilder builder = new StringBuilder();
-        // Arrays.sort(strs);
-        // char[] s1 = strs[0].toCharArray();
-        // char[] s2 = strs[strs.length - 1].toCharArray();
-        // int n = Math.min(s1.length, s2.length);
-        // for (int i = 0; i < n; i++) {
-        //     if (s1[i] == s2[i]) {
-        //         builder.append(s1[i]);
-        //     } else {
-        //         return builder.toString();
-        //     }
-        // }
-        // return builder.toString();
-
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-
-        String prefix = strs[0];
-
-        for (int i = 1; i < strs.length; i++) {
-            while(!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() -1);
-                if(prefix == "") {
-                    return "";
-                }
+        StringBuilder builder = new StringBuilder();
+        Arrays.sort(strs);
+        char[] s1 = strs[0].toCharArray();
+        char[] s2 = strs[strs.length - 1].toCharArray();
+        int n = Math.min(s1.length, s2.length);
+        for (int i = 0; i < n; i++) {
+            if (s1[i] == s2[i]) {
+                builder.append(s1[i]);
+            } else {
+                return builder.toString();
             }
         }
+        return builder.toString();
 
-        return prefix;
+        // if (strs == null || strs.length == 0) {
+        //     return "";
+        // }
+
+        // String prefix = strs[0];
+
+        // for (int i = 1; i < strs.length; i++) {
+        //     while(!strs[i].startsWith(prefix)) {
+        //         prefix = prefix.substring(0, prefix.length() -1);
+        //         if(prefix == "") {
+        //             return "";
+        //         }
+        //     }
+        // }
+
+        // return prefix;
     }
 }
