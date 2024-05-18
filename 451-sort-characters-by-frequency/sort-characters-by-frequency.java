b1fe0ba13bcd36acc353082b840e17df66c0830f
Sandeep <sandeepg2404@gmail.com>
2024-05-18 22:16:54 +0530

    Time: 13 ms (74.79%) | Memory: 46 MB (9.63%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -0,0 +1,37 @@
+class Solution {
+    private static int CHAR = 256;
+
+    public String frequencySort(String s) {
+        int[] freqArr = new int[256];
+        StringBuilder res = new StringBuilder(s.length());
+        for (char ch : s.toCharArray()) {
+            freqArr[ch]++;
+        }
+        List<List<Character>> buckets = Stream.generate(() -> new ArrayList<Character>())
+                                        .limit(s.length() + 1)
+                                        .collect(Collectors.toList());
+
+        for (int i = 0; i < CHAR; i++) {
+            char ch = (char) i;
+            int freq = freqArr[i];
+            if (freq != 0) {
+                List<Character> chars = buckets.get(freq);
+                chars.add(ch);
+            }
+        }
+
+        for (int i = buckets.size() - 1; i >= 0; i--) {
+            List<Character> chars = buckets.get(i);
+            for (int j = 0; j < chars.size(); j++) {
+                char ch = chars.get(j);
+                int cnt = i;
+                while(cnt != 0) {
+                    res.append(ch);
+                    cnt--;
+                }
+            }
+        }
+        return res.toString();
+    }
+    
+}
