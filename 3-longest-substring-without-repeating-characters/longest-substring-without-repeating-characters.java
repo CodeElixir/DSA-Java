8f220892fc61b0cf98473d4671cc43aa44f237fc
Sandeep <sandeepg2404@gmail.com>
2024-05-20 22:55:56 +0530

    Time: 5 ms (86.99%) | Memory: 44.9 MB (20.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,31 +1,55 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int[] freq = new int[128];
+        // int[] freq = new int[128];
+        // int left = 0;
+        // int right = 0;
+        // int maxLen = 0;
+        // char[] chars = s.toCharArray();
+        // for (int i = 0; i < chars.length; i++) {
+        //     freq[chars[i]]++;
+        //     if (isDistinct(freq)) {
+        //         right++;
+        //     } else {
+        //         maxLen = Math.max(maxLen, right - left);
+        //         while (!isDistinct(freq)) {
+        //             freq[chars[left++]]--;
+        //         }
+        //         right++;  
+        //     }
+        // }
+        // return maxLen = Math.max(maxLen, right - left);
+
+        // if (s.length() <= 1) {
+        //     return s.length();
+        // }
+
+        Map<Character, Integer> map = new HashMap<>();
         int left = 0;
-        int right = 0;
+        // int right = 0;
         int maxLen = 0;
         char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            freq[chars[i]]++;
-            if (isDistinct(freq)) {
-                right++;
-            } else {
-                maxLen = Math.max(maxLen, right - left);
-                while (!isDistinct(freq)) {
-                    freq[chars[left++]]--;
+        int i;
+        for (i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            if (map.containsKey(ch)) {
+                int prevIdx = map.get(ch);
+                if (prevIdx >= left) {
+                    maxLen = Math.max(maxLen, i - left);
+                    left = prevIdx + 1;
                 }
-                right++;  
             }
+            map.put(ch, i);
+            // right++
         }
-        return maxLen = Math.max(maxLen, right - left);
+        return Math.max(maxLen, i - left);
     }
 
-    private boolean isDistinct(int[] freq) {
-        for (int i : freq) {
-            if (i != 0 && i != 1) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // private boolean isDistinct(int[] freq) {
+    //     for (int i : freq) {
+    //         if (i != 0 && i != 1) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
