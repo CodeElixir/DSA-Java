0acb4ca44525ac6f0f433499605e7cb10b07dc87
Sandeep <sandeepg2404@gmail.com>
2024-05-21 00:15:07 +0530

    Time: 3 ms (84.11%) | Memory: 46.3 MB (94.43%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,27 +1,49 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int left = 0;
+        // int left = 0;
+        // int cnt = 0;
+        // int max = 0;
+        // int i;
+        // for (i = 0; i < nums.length; i++) {
+        //     if (cnt == k && nums[i] == 0) {
+        //         max = Math.max(max, i - left);
+        //         while (cnt != k - 1) {
+        //             if (nums[left++] == 0) {
+        //                 cnt--;
+        //             }
+        //         } 
+        //     }
+
+        //     if (nums[i] == 0) {
+        //         cnt++;
+        //     }
+        // }
+
+        // if (cnt != k && cnt > 0) {
+        //     return nums.length;
+        // }
+        // return Math.max(max, i - left);
+                int left = 0;
         int cnt = 0;
         int max = 0;
         int i;
+
         for (i = 0; i < nums.length; i++) {
-            if (cnt == k && nums[i] == 0) {
-                max = Math.max(max, i - left);
-                while (cnt != k - 1) {
-                    if (nums[left++] == 0) {
-                        cnt--;
-                    }
-                } 
-            }
 
             if (nums[i] == 0) {
                 cnt++;
             }
-        }
 
-        if (cnt != k && cnt > 0) {
-            return nums.length;
+            while (cnt > k) {
+                if (nums[left++] == 0) {
+                    cnt--;
+                }
+            }
+
+            if (cnt <= k) {
+                max = Math.max(max, i - left + 1);
+            }
         }
-        return Math.max(max, i - left);
+        return max;
     }
 }
