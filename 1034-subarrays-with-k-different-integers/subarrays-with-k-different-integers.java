fd06bd65b22afd6dc8f1cd42fc5c1be2a6c97d39
Sandeep <sandeepg2404@gmail.com>
2024-05-22 21:47:30 +0530

    Time: 5 ms (89.39%) | Memory: 47.1 MB (64.94%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -8,22 +8,49 @@ class Solution {
             return 0;
         }
         
-        int cnt = 0;
+        int res = 0;
         int left = 0;
-        Map<Integer,Integer> map = new HashMap<>(nums.length);
+        int cnt = 0;
+        int[] freq = new int[nums.length + 1];
         
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
-                }
-                left++;
+            freq[nums[i]]++;
+            if (freq[nums[i]] == 1) {
+                cnt++;
             }
-            cnt = cnt + (i - left + 1);
+            while (cnt > k) {
+                freq[nums[left]]--;
+                if (freq[nums[left++]] == 0) {
+                    cnt--;
+                }
+            }
+            res = res + (i - left + 1);
         }
 
-        return cnt;
+        return res;
     }
+
+    // private int subarraysWithLesserThanEqualKDistinct(int[] nums, int k) {
+    //     if (k < 0) {
+    //         return 0;
+    //     }
+        
+    //     int cnt = 0;
+    //     int left = 0;
+    //     Map<Integer,Integer> map = new HashMap<>(nums.length);
+        
+    //     for (int i = 0; i < nums.length; i++) {
+    //         map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    //         while (map.size() > k) {
+    //             map.put(nums[left], map.get(nums[left]) - 1);
+    //             if (map.get(nums[left]) == 0) {
+    //                 map.remove(nums[left]);
+    //             }
+    //             left++;
+    //         }
+    //         cnt = cnt + (i - left + 1);
+    //     }
+
+    //     return cnt;
+    // }
 }
