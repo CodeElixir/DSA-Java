fd62982fcec9263a1fc90506e758ba476a453079
Sandeep <sandeepg2404@gmail.com>
2024-05-22 19:49:29 +0530

    Time: 11 ms (90.45%) | Memory: 44.6 MB (34.24%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -12,4 +12,27 @@ class Solution {
         }
         return cnt;
     }
+
+    // public static int countSubstring(String s){
+    //     // Write your code here.
+    //     // to find the tot subarray count
+    //     int res = s.length()*(s.length()+1)/2;
+    //     // return tot subarray - subarray which has atmost any 2 characters from a,b,c.
+    //     return res-solve(s);
+    // }
+    // // function to find a subarray which has atmost any 2 character from a,b,c
+    // public static int solve(String s){
+    //     HashMap<Character,Integer> map = new HashMap<>();
+    //     int left=0,count=0;
+    //     for(int right=0;right<s.length();right++){
+    //         map.put(s.charAt(right),map.getOrDefault(s.charAt(right), 0)+1);
+    //         while(map.size()>2){
+    //             map.put(s.charAt(left), map.get(s.charAt(left))-1);
+    //             if(map.get(s.charAt(left))==0) map.remove(s.charAt(left));
+    //             left++;
+    //         }
+    //         count+=right-left+1;
+    //     }
+    //     return count;
+    // }
 }
