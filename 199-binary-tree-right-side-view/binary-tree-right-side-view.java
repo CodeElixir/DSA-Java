b54fe46b617987ca3e08f5f82c26851d8d151934
Sandeep <sandeepg2404@gmail.com>
2024-06-06 18:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (95.55%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,29 +15,49 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        // Level Order
+        // List<Integer> list = new ArrayList<>();
+        // if (root == null) {
+        //     return list;
+        // }
+        // Deque<TreeNode> queue = new ArrayDeque<>();
+        // queue.addLast(root);
+        // while (!queue.isEmpty()) {
+        //     int count = queue.size();
+        //     for (int i = 0; i < count; i++) {
+        //         TreeNode node = queue.removeFirst();
+        //         if (i == count - 1) {
+        //             list.add(node.val);
+        //         }
+        //         if (node.left != null) {
+        //             queue.addLast(node.left);
+        //         }
+
+        //         if (node.right != null) {
+        //             queue.addLast(node.right);
+        //         }
+
+        //     }
+        // }
+        // return list;
+
+        // Reverse Preorder (Root Right Left)
         List<Integer> list = new ArrayList<>();
-        if (root == null) {
-            return list;
-        }
-        Deque<TreeNode> queue = new ArrayDeque<>();
-        queue.addLast(root);
-        while (!queue.isEmpty()) {
-            int count = queue.size();
-            for (int i = 0; i < count; i++) {
-                TreeNode node = queue.removeFirst();
-                if (i == count - 1) {
-                    list.add(node.val);
-                }
-                if (node.left != null) {
-                    queue.addLast(node.left);
-                }
-
-                if (node.right != null) {
-                    queue.addLast(node.right);
-                }
-
-            }
-        }
+        reversePreorder(root, 0, list);
         return list;
+    }
+
+    private void reversePreorder(TreeNode root, int level, List<Integer> rightSideViewNodes) {
+        if (root == null) {
+            return;
+        }
+
+        if (rightSideViewNodes.size() == level) {
+            rightSideViewNodes.add(root.val);
+        }
+
+        reversePreorder(root.right, level + 1, rightSideViewNodes);
+        reversePreorder(root.left, level + 1, rightSideViewNodes);
+
     }
 }
