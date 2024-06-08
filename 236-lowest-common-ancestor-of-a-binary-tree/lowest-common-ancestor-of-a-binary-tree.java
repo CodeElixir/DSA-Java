da78d927245ab1f5cf554c23cbd03193206a3a36
Sandeep <sandeepg2404@gmail.com>
2024-06-08 13:03:22 +0530

    Time: 7 ms (62.71%) | Memory: 44.8 MB (39.97%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -11,30 +11,30 @@ class Solution {
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // return lcaHelper(root, p, q);
-        if(root == null || root == p || root == q) {
-            return root;
-        }
+        return lcaHelper(root, p, q);
+        // if(root == null || root == p || root == q) {
+        //     return root;
+        // }
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if(left != null && right != null) {
-            return root;
-        } else if(left != null) {
-            return left;
-        } else if(right != null) {
-            return right;
-        }
-        return null;
+        // if(left != null && right != null) {
+        //     return root;
+        // } else if(left != null) {
+        //     return left;
+        // } else if(right != null) {
+        //     return right;
+        // }
+        // return null;
     }
 
     private TreeNode lcaHelper(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        }
+        // if (root == null) {
+        //     return null;
+        // }
 
-        if (root == p || root == q) {
+        if (root == null || root == p || root == q) {
             return root;
         }
 
