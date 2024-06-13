68748f8ed23ae6dd09133807726767bbdb1d8905
Sandeep <sandeepg2404@gmail.com>
2024-06-13 11:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (85.53%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -20,12 +20,58 @@ class Solution {
         return inorder;
     }
 
+    // private void inorderTraversalHelper(List<Integer> inorder, TreeNode root) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     inorderTraversalHelper(inorder, root.left);
+    //     inorder.add(root.val);
+    //     inorderTraversalHelper(inorder, root.right);
+    // }
+
+    // Iterative
+    // private void inorderTraversalHelper(List<Integer> inorder, TreeNode root) {
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     TreeNode node = root;
+
+    //     while (true) {
+    //         if (node != null) {
+    //             stack.push(node);
+    //             node = node.left;
+    //         } else {
+    //             if (stack.isEmpty()) {
+    //                 break;
+    //             }
+    //             node = stack.pop();
+    //             inorder.add(node.val);
+    //             node = node.right;
+    //         }
+    //     }
+    // }
+
+    // Morris inorder traversal
     private void inorderTraversalHelper(List<Integer> inorder, TreeNode root) {
-        if (root == null) {
-            return;
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                inorder.add(curr.val);
+                curr = curr.right;
+            } else {
+                // Find the rightmost node in this sub tree
+                TreeNode rightNode = curr.left;
+                while (rightNode.right != null && rightNode.right != curr) {
+                    rightNode = rightNode.right;
+                }
+
+                if (rightNode.right == null) {
+                    rightNode.right = curr;
+                    curr = curr.left;
+                } else {
+                    rightNode.right = null;
+                    inorder.add(curr.val);
+                    curr = curr.right;
+                }
+            }
         }
-        inorderTraversalHelper(inorder, root.left);
-        inorder.add(root.val);
-        inorderTraversalHelper(inorder, root.right);
     }
 }
